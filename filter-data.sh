65c7cf6aac4359353ccc643c436dd19c4ae8d0a5
Sri Vignesh <sselvan@redhat.com>
2024-01-20 11:43:48 +0530

    Update filter-data.sh

--- filter-data.sh
+++ filter-data.sh
@@ -2,23 +2,15 @@
 set -euo pipefail
 
 authors='[
-  "srivickynesh",
-  "avi-biton",
-  "yftacherzog",
-  "Kousalya1998",
-  "Omeramsc",
-  "ShakedAviv50313",
-  "hmariset",
-  "eisraeli",
-  "gbenhaim",
-  "amisstea"
+  "srivickynesh"
 ]'
 
 # Assumes data from stdin!
 jq --argjson authors "${authors}" -r '[
     .[] | select(
-        ([.user] | inside($authors)) or
-        (.url | contains("/o11y/")) or
-        (.title | test("o11y"; "i"))
+        ([.user] | inside($authors))
+        # or
+        # (.url | contains("/o11y/")) or
+        # (.title | test("o11y"; "i"))
     )
 ]'
